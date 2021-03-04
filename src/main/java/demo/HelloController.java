83220725d4b91c0309d3550cc9a7162f29d5720d
yarde <yarden.barak@s.afeka.ac.il>
2021-03-04 11:41:12 +0200

    created new project

---
+++ src/main/java/demo/HelloController.java
@@ -0,0 +1,50 @@
+package demo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+//	private int counter; // ERROR!! DO NOT USE SUCH IMPLEMENTATION IN YOUR CODE
+	
+	private AtomicInteger counter;
+	
+	public HelloController() {
+		this.counter = new AtomicInteger(1);
+	}
+	
+	@RequestMapping(
+		path = "/hello",
+		method = RequestMethod.GET,
+		produces = MediaType.APPLICATION_JSON_VALUE)
+	public HelloBoundary hello() {
+		HelloBoundary rv = new HelloBoundary("Hello world!", counter.getAndIncrement());
+		System.err.println(rv);
+		return rv;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
