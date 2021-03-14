e59b3c49f10e8bfd28acdfdd911f24a5b93fe73a
johny <johny@10.0.0.5>
2021-03-14 14:36:27 +0200

    add newUserDetails , get User from server

---
+++ src/main/java/demo/models/NewUserDetails.java
@@ -0,0 +1,66 @@
+package demo.models;
+
+public class NewUserDetails {
+	String email;
+	String role;
+	String username;
+	String avatar;
+	
+	public NewUserDetails(){
+	}
+	
+	public NewUserDetails(String email, String role, String username, String avatar) {
+		super();
+		this.email = email;
+		this.role = role;
+		this.username = username;
+		this.avatar = avatar;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	@Override
+	public String toString() {
+		return "NewUserDetails : email= " + email + ", role= " + role + ", username= " + username + ", avatar= " + avatar;
+	}
+	
+	
+}
