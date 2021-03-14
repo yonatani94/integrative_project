1b51346ebe7ada2339115048789723b7a9dcf354
nathan amiel <nanathanamiel5@gmail.com>
2021-03-14 15:26:31 +0200

    add itemBoundary

--- src/main/java/demo/boundarys/ItemBoundary.java
+++ src/main/java/demo/boundarys/ItemBoundary.java
@@ -1,6 +1,7 @@
 package demo.boundarys;
 
 import demo.models.CreatedBy;
+import demo.models.ItemAttributes;
 import demo.models.ItemID;
 import demo.models.Location;
 import demo.models.OperationId;
@@ -14,7 +15,93 @@ public class ItemBoundary {
 	private String createdTimestamp;
 	private CreatedBy createdBy;
 	private Location location;
-	//private ItemAttributes itemAttributes;
+	private ItemAttributes itemAttributes;
+	
+	public ItemBoundary(ItemID itemID, String type, String name, boolean active, String createdTimestamp,
+			CreatedBy createdBy, Location location, ItemAttributes itemAttributes) {
+		super();
+		this.itemID = itemID;
+		this.type = type;
+		this.name = name;
+		this.active = active;
+		this.createdTimestamp = createdTimestamp;
+		this.createdBy = createdBy;
+		this.location = location;
+		this.itemAttributes = itemAttributes;
+	}
+
+	public ItemID getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(ItemID itemID) {
+		this.itemID = itemID;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getCreatedTimestamp() {
+		return createdTimestamp;
+	}
+
+	public void setCreatedTimestamp(String createdTimestamp) {
+		this.createdTimestamp = createdTimestamp;
+	}
+
+	public CreatedBy getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(CreatedBy createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public ItemAttributes getItemAttributes() {
+		return itemAttributes;
+	}
+
+	public void setItemAttributes(ItemAttributes itemAttributes) {
+		this.itemAttributes = itemAttributes;
+	}
+
+	@Override
+	public String toString() {
+		return "ItemBoundary [itemID=" + itemID + ", type=" + type + ", name=" + name + ", active=" + active
+				+ ", createdTimestamp=" + createdTimestamp + ", createdBy=" + createdBy + ", location=" + location
+				+ ", itemAttributes=" + itemAttributes + "]";
+	}
+	
+	
 	
 	
 }
--- src/main/java/demo/models/Location.java
+++ src/main/java/demo/models/Location.java
@@ -1,15 +1,34 @@
 package demo.models;
 
 public class Location {
-	
+
 	private double lat;
 	private double log;
+
+	public Location() {
+		super();
+	}
+
 	public Location(double lat, double log) {
 		super();
 		this.lat = lat;
 		this.log = log;
 	}
-	
-	
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLog() {
+		return log;
+	}
+
+	public void setLog(double log) {
+		this.log = log;
+	}
 
 }
