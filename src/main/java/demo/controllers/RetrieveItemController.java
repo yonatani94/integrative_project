70d57d5d48b9102c8bb8a47c6f516722dbd3a059
nathan amiel <nanathanamiel5@gmail.com>
2021-03-17 14:57:38 +0200

    add retriveitem

--- src/main/java/demo/controllers/RetrieveItemController.java
+++ src/main/java/demo/controllers/RetrieveItemController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.boundarys.ItemBoundary;
+import demo.models.CreatedBy;
+import demo.models.ItemAttributes;
+import demo.models.ItemID;
+import demo.models.Location;
+import demo.models.UserID;
 
 @RestController
 public class RetrieveItemController {
@@ -17,7 +22,8 @@ public class RetrieveItemController {
 			@PathVariable("userEmail") String userEmail) {
 
 
-		ItemBoundary rv = new ItemBoundary(null, userEmail, userEmail, false, userEmail, null, null, null);
+		ItemBoundary rv = new ItemBoundary(new ItemID(userSpace,"99"), "demoType", "demo Item", true, "2021-03-07T09:55:05.248+0000", new CreatedBy(new UserID(userSpace,userEmail)), new Location(32.115139,34.817804), new ItemAttributes("can be set to any value you wish",
+				"you can also name the attributes any name you like",58,false));
 		System.out.println(rv);
 		return rv;
 	}
