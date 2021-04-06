e9dc08449a5382e9cf57a951501b05b5cf8fbe8e
Vadim Kandaurov <Vadim.Kandorov@s.afeka.ac.il>
2021-04-06 17:59:56 +0300

    Added ItemsService

--- src/main/java/twins/data/OperationEntity.java
+++ src/main/java/twins/data/OperationEntity.java
@@ -1,5 +1,91 @@
 package twins.data;
 
-public class OperationEntity {
+import java.util.Date;
+import java.util.Map;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import restapi.models.InvokedBy;
+import restapi.models.Item;
+import restapi.models.ItemID;
+import restapi.models.OperationId;
+
+@Entity
+@Table(name="OPERATIONS")
+public class OperationEntity {
+	
+	private OperationId operationId;
+	private String type;
+	private Item item;
+	private Date createdTimestamp;
+	private InvokedBy invokedBy;
+	private Map<String, Object> operationAttributes;
+	
+	public OperationEntity() {
+		
+	}
+
+	@Id
+	public OperationId getOperationId() {
+		return operationId;
+	}
+
+	public void setOperationId(OperationId operationId) {
+		this.operationId = operationId;
+	}
+
+	@Transient
+	public String getType() {
+		return type;
+	}
+
+	@Transient
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Transient
+	public Item getItem() {
+		return item;
+	}
+	
+	@Transient
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	
+	@Transient
+	public Date getCreatedTimestamp() {
+		return createdTimestamp;
+	}
+
+	@Transient
+	public void setCreatedTimestamp(Date createdTimestamp) {
+		this.createdTimestamp = createdTimestamp;
+	}
+
+	@Transient
+	public InvokedBy getInvokedBy() {
+		return invokedBy;
+	}
+
+	@Transient
+	public void setInvokedBy(InvokedBy invokedBy) {
+		this.invokedBy = invokedBy;
+	}
+	
+	@Transient
+	public Map<String, Object> getOperationAttributes() {
+		return operationAttributes;
+	}
+	
+	@Transient
+	public void setOperationAttributes(Map<String, Object> operationAttributes) {
+		this.operationAttributes = operationAttributes;
+	}
+
+	
 }
---
+++ src/main/java/twins/logic/ItemsService.java
@@ -0,0 +1,18 @@
+package twins.logic;
+
+import java.util.List;
+
+import restapi.boundarys.ItemBoundary;
+
+public interface ItemsService {
+	public ItemBoundary createItem(String userSpace, String userEmail, ItemBoundary item);
+
+	public ItemBoundary updateItem(String userSpace, String userEmail, String itemSpace, String itemId,
+			ItemBoundary update);
+
+	public List<ItemBoundary> getAllItems(String userSpace, String userEmail);
+
+	public ItemBoundary getSpecificItem(String userSpace, String userEmail, String itemSpace, String itemId);
+
+	public void deleteAllItems(String adminSpace, String adminEmail);
+}
